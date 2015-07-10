7b29b23f950d54f91c78cc4a5cfb4e15f8f8799f
defektive <sirbradleyd@gmail.com>
2015-07-10 08:59:26 -0600

    Added links to github

--- react-frontend/src/app/pages/Home.js
+++ react-frontend/src/app/pages/Home.js
@@ -7,6 +7,13 @@ export default class Home extends React.Component {
       <div>
         <h2>Home</h2>
         <WelcomeBox />
+
+        <div>
+          <a href="https://github.com/defektive/react-rest-sample">Github Repo</a>
+          <a href="https://github.com/defektive/react-frontend">React Frontend</a>
+          <a href="https://github.com/defektive/rails-templates">Rails Templates</a>
+
+        </div>
       </div>
     );
   }
